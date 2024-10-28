8461426aea9dabe82179a1408a114b89d260bde4
qqa108 <qqa109@naver.com>
2024-10-28 11:04:54 +0900

    init be-develop

---
+++ backend/src/main/java/sleepNavigator/global/utils/UserLoginService.java
@@ -0,0 +1,16 @@
+package sleepNavigator.global.utils;
+
+
+// 임시로 설정함 삭제 예정
+public class UserLoginService {
+    public String getRefreshTokenByUserId(int userId) {
+        return "";
+    }
+    public int getUserIdByRefreshToken(String refreshToken) {
+        return 0;
+    }
+    public void invalidateRefreshToken(int userId) {
+
+    }
+
+}
