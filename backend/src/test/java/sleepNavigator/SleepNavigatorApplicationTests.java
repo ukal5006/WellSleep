66af9951445e5170ce170b907cf079191df2f909
정경원 <ekdhfksl12@naver.com>
2024-10-28 10:00:59 +0900

    feat: init 세팅

---
+++ backend/src/test/java/sleepNavigator/SleepNavigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package sleepNavigator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleepNavigatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
