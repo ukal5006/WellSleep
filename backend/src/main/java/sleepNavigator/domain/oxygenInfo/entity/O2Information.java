dcab874462a93cae42b65265d6e5586f6d8bd4f5
qqa108 <qqa109@naver.com>
2024-10-29 13:32:19 +0900

    new colum is_sleep

--- backend/src/main/java/sleepNavigator/domain/oxygenInfo/entity/O2Information.java
+++ backend/src/main/java/sleepNavigator/domain/oxygenInfo/entity/O2Information.java
@@ -28,4 +28,7 @@ public class O2Information {
 
     @Column (name = "value", nullable = false)
     private double value;
+
+    @Column (name = "is_sleep", nullable = false)
+    private boolean isSleep;
 }
