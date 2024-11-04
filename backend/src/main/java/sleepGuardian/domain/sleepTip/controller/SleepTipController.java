7b5d38e59f73fff724db1e9a90b65a152182312b
정경원 <ekdhfksl12@naver.com>
2024-11-05 02:02:58 +0900

    feat: 수면 지원 기능 추가

--- backend/src/main/java/sleepGuardian/domain/sleepTip/controller/SleepTipController.java
+++ backend/src/main/java/sleepGuardian/domain/sleepTip/controller/SleepTipController.java
@@ -1,11 +1,37 @@
 package sleepGuardian.domain.sleepTip.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sleepGuardian.domain.sleepTip.dto.SleepTipResponseDTO;
+import sleepGuardian.domain.sleepTip.entity.Category;
+import sleepGuardian.domain.sleepTip.entity.SleepTip;
 import sleepGuardian.domain.sleepTip.service.SleepTipService;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
+@RequestMapping("/api/support")
 @RequiredArgsConstructor
 public class SleepTipController {
     private final SleepTipService sleepTipService;
+
+    @GetMapping("/tip")
+    public List<SleepTipResponseDTO> getTips() {
+        List<SleepTip> tips = sleepTipService.getTipsByCategory(Category.팁);
+        return tips.stream()
+                .map(SleepTipResponseDTO::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/foods")
+    public List<SleepTipResponseDTO> getFoods() {
+        List<SleepTip> foods = sleepTipService.getTipsByCategory(Category.음식);
+        return foods.stream()
+                .map(SleepTipResponseDTO::fromEntity)
+                .collect(Collectors.toList());
+    }
+
 }
---
+++ backend/src/main/java/sleepGuardian/domain/sleepTip/dto/SleepTipResponseDTO.java
@@ -0,0 +1,29 @@
+package sleepGuardian.domain.sleepTip.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import sleepGuardian.domain.sleepTip.entity.SleepTip;
+
+@Getter
+@AllArgsConstructor
+public class SleepTipResponseDTO {
+    private int id;
+    private int category;
+    private String title;
+    private String content;
+    private String image;
+    private String created_at;
+    private String updated_at;
+
+    public static SleepTipResponseDTO fromEntity(SleepTip sleepTip) {
+        return new SleepTipResponseDTO(
+                sleepTip.getId(),
+                sleepTip.getCategory().ordinal() + 1, // Enum 인덱스 기반
+                sleepTip.getTitle(),
+                sleepTip.getContent(),
+                sleepTip.getImage(),
+                sleepTip.getCreatedAt().toString(),
+                sleepTip.getUpdatedAt() != null ? sleepTip.getUpdatedAt().toString() : null
+        );
+    }
+}
