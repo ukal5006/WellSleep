2a869a67cbe6f639541919baeb00c855f09a2164
정경원 <ekdhfksl12@naver.com>
2024-11-05 22:52:38 +0900

    feat: 유튜브 기능 추가

--- backend/src/main/java/sleepGuardian/domain/sleepTip/controller/SleepTipController.java
+++ backend/src/main/java/sleepGuardian/domain/sleepTip/controller/SleepTipController.java
@@ -1,14 +1,19 @@
 package sleepGuardian.domain.sleepTip.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import sleepGuardian.domain.sleepTip.dto.SleepTipResponseDTO;
+import sleepGuardian.domain.sleepTip.dto.SleepVideoResponseDTO;
 import sleepGuardian.domain.sleepTip.entity.Category;
 import sleepGuardian.domain.sleepTip.entity.SleepTip;
 import sleepGuardian.domain.sleepTip.service.SleepTipService;
+import sleepGuardian.domain.sleepTip.service.SleepVideoService;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,21 +22,32 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class SleepTipController {
     private final SleepTipService sleepTipService;
+    private final SleepVideoService sleepVideoService;
 
     @GetMapping("/tip")
-    public List<SleepTipResponseDTO> getTips() {
+    public ResponseEntity<?> getTips() {
         List<SleepTip> tips = sleepTipService.getTipsByCategory(Category.팁);
-        return tips.stream()
+        List<SleepTipResponseDTO> response = tips.stream()
                 .map(SleepTipResponseDTO::fromEntity)
                 .collect(Collectors.toList());
+
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/foods")
-    public List<SleepTipResponseDTO> getFoods() {
+    public ResponseEntity<?> getFoods() {
         List<SleepTip> foods = sleepTipService.getTipsByCategory(Category.음식);
-        return foods.stream()
+        List<SleepTipResponseDTO> response = foods.stream()
                 .map(SleepTipResponseDTO::fromEntity)
                 .collect(Collectors.toList());
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/videos")
+    public ResponseEntity<?> getVideos(@RequestParam String keyword) {
+        SleepVideoResponseDTO sleepVideoResponseDTO = sleepVideoService.searchVideo(keyword);
+        return ResponseEntity.ok(sleepVideoResponseDTO);
     }
 
 }
