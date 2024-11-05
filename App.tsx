730b7c6c944705a300db535878fde4a6039330dd
Leo Lee <ukal5006@naver.com>
2024-11-05 13:17:59 +0900

    feat : 페이지 이동 기능 구현

--- App.tsx
+++ App.tsx
@@ -3,6 +3,14 @@ import { login } from '@react-native-kakao/user';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const StyledText = styled(Text)`
+    background-color: tomato;
+`;
 
 export default function App() {
     useEffect(() => {
@@ -16,6 +24,7 @@ export default function App() {
                     login();
                 }}
             />
+            <StyledText>테스트</StyledText>
             <StatusBar style="auto" />
         </View>
     );
