bed403c6d480a18ecd71de3e19700d201fa0ecad
unknown <ski03259@naver.com>
2024-11-06 13:40:55 +0900

    design: 로그인 버튼

--- components/LoginBtn.tsx
+++ components/LoginBtn.tsx
@@ -1,55 +1,68 @@
-import { login } from '@react-native-kakao/user';
-import { Text, TouchableOpacity } from 'react-native';
-import styled from 'styled-components/native'; // styled-components/native로 변경
-import * as SecureStore from 'expo-secure-store';
-import { Alert } from 'react-native';
-import { useEffect } from 'react';
-import useAxios from '../hooks/useAxios';
-import { LOGIN } from '../constants/apis';
-import { KAKAOYELLOW } from '../constants/colors';
+import { login } from "@react-native-kakao/user";
+import { Text, TouchableOpacity, Image } from "react-native";
+import styled from "styled-components/native"; // styled-components/native로 변경
+import * as SecureStore from "expo-secure-store";
+import { Alert } from "react-native";
+import { useEffect } from "react";
+import useAxios from "../hooks/useAxios";
+import { LOGIN } from "../constants/apis";
+import { KAKAOYELLOW } from "../constants/colors";
 
 const KakaoBtn = styled(TouchableOpacity)`
-    font-size: 24px;
-    padding: 10px 20px;
-    border-radius: 15px;
-    width: 90%;
-    background-color: ${KAKAOYELLOW};
-    display: flex;
-    justify-content: center;
+  flex-direction: row;
+  align-items: center;
+
+  padding: 10px 20px;
+  border-radius: 18px;
+  width: 371px;
+  height: 63px;
+  background-color: ${KAKAOYELLOW};
+`;
+
+const KakaoText = styled(Text)`
+  font-size: 17px;
+`;
+
+const KakaoImage = styled(Image)`
+  width: 28px;
+  height: 28px;
+  margin-right: 70px;
+  margin-left: 20px;
 `;
 
 async function storeTokens(accessToken: string, refreshToken: string) {
-    await SecureStore.setItemAsync('accessToken', accessToken);
-    await SecureStore.setItemAsync('refreshToken', refreshToken);
+  await SecureStore.setItemAsync("accessToken", accessToken);
+  await SecureStore.setItemAsync("refreshToken", refreshToken);
 }
 
 function LoginBtn() {
-    const { dataFetch, loading, error, data } = useAxios();
-    const handleLogin = async () => {
-        try {
-            const result = await login();
-            const kakaoToken: string = result.accessToken;
-            console.log('카카오 로그인 성공');
-            await dataFetch('POST', LOGIN, { kakaoToken: kakaoToken });
-        } catch (error) {
-            console.error('로그인 실패:', error);
-            Alert.alert('로그인 실패', '로그인에 실패했습니다. 다시 시도하세요.');
-        }
-    };
+  const { dataFetch, loading, error, data } = useAxios();
+  const handleLogin = async () => {
+    try {
+      const result = await login();
+      const kakaoToken: string = result.accessToken;
+      console.log("카카오 로그인 성공");
+      await dataFetch("POST", LOGIN, { kakaoToken: kakaoToken });
+    } catch (error) {
+      console.error("로그인 실패:", error);
+      Alert.alert("로그인 실패", "로그인에 실패했습니다. 다시 시도하세요.");
+    }
+  };
 
-    useEffect(() => {
-        if (data !== null) {
-            storeTokens(data.accessToken, data.refreshToken);
-            console.log(data);
-            console.log('WellSleep 로그인 성공');
-        }
-    }, [data]);
+  useEffect(() => {
+    if (data !== null) {
+      storeTokens(data.accessToken, data.refreshToken);
+      console.log(data);
+      console.log("WellSleep 로그인 성공");
+    }
+  }, [data]);
 
-    return (
-        <KakaoBtn onPress={handleLogin}>
-            <Text>카카오로 시작하기</Text>
-        </KakaoBtn>
-    );
+  return (
+    <KakaoBtn onPress={handleLogin}>
+      <KakaoImage source={require("../assets/kakao.png")} />
+      <KakaoText>카카오로 시작하기</KakaoText>
+    </KakaoBtn>
+  );
 }
 
 export default LoginBtn;
