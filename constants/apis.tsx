9f4645dd97160710be5c221f8bbfbbb6f919c417
Leo Lee <ukal5006@naver.com>
2024-11-06 10:24:59 +0900

    feat : 로그인 구현

---
+++ constants/apis.tsx
@@ -0,0 +1,3 @@
+const DOMAIN = 'https://k11b108.p.ssafy.io/api/';
+export const LOGIN = `${DOMAIN}login`;
+export const LOGOUT = `${DOMAIN}logout`;
---
+++ hooks/useAxios.tsx
@@ -0,0 +1,47 @@
+// hooks/useAxios.ts
+import { useState, useEffect } from 'react';
+import axios, { AxiosRequestConfig, AxiosError } from 'axios';
+import * as SecureStore from 'expo-secure-store';
+
+const useAxios = () => {
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<any>(null);
+    const [data, setData] = useState<any>(null);
+
+    const dataFetch = async (method: 'GET' | 'POST' | 'PUT' | 'DELETE', url: string, body: any = null) => {
+        setLoading(true);
+        setError(null);
+
+        try {
+            // SecureStore에서 토큰 가져오기
+            const accessToken = await SecureStore.getItemAsync('accessToken');
+            const refreshToken = await SecureStore.getItemAsync('refreshToken');
+
+            // 고정 헤더 설정
+            const headers = {
+                Authorization: accessToken,
+                RefreshToken: refreshToken,
+            };
+
+            const config: AxiosRequestConfig = {
+                method,
+                url,
+                headers,
+                data: body,
+            };
+            console.log('보낸요청');
+            console.log(config);
+            const response = await axios(config);
+            setData(response.data); // 요청 성공 시 데이터 저장
+            // console.log(response.data);
+        } catch (err) {
+            setError(err);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return { dataFetch, loading, error, data };
+};
+
+export default useAxios;
