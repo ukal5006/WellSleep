22bdd0b50e56563c93ed46d1b7182054d78fb124
Leo Lee <ukal5006@naver.com>
2024-11-13 11:26:12 +0900

    fix : 깃 충돌 해결

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -1,41 +1,65 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
-import Login from "../screens/Login";
-import Main from "../screens/Main";
-import Mypage from "../screens/Mypage";
-import MonthlyChart from "../screens/Chart/MonthlyChart";
-import Exampage from "../screens/Exampage";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Mypage from '../screens/Mypage';
+import MonthlyChart from '../screens/Chart/MonthlyChart';
+import Exampage from '../screens/Exampage';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
+import { USER } from '../constants/apis';
+import useAxios from '../hooks/useAxios';
+import { setUserInfo } from '../store/userSlice';
+import Main from '../screens/Main/Main';
 
 const Tab = createBottomTabNavigator();
 
 function NavBar() {
-  return (
-    <Tab.Navigator
-      screenOptions={{
-        tabBarStyle: {
-          backgroundColor: "#8B728F",
-          borderTopWidth: 0,
-          height: 70, // 탭 바의 높이 설정
-        },
-        tabBarLabelStyle: {
-          color: "#ffffff", // 글씨 색상
-          fontSize: 12, // 원하는 글씨 크기로 변경
-          fontWeight: "bold", // 글씨 두께
-        },
-        tabBarItemStyle: {
-          paddingVertical: 10, // 각 탭의 수직 패딩
-        },
-        tabBarActiveTintColor: "#FFFFFF", // 선택된 탭의 색상
-        tabBarInactiveTintColor: "#C6BAC8", // 비활성 탭의 색상
-        headerShown: false, // 모든 스크린에서 헤더를 숨기려면 추가
-      }}
-    >
-      <Tab.Screen name="홈" component={Login} />
-      <Tab.Screen name="수면일지" component={MonthlyChart} />
-      <Tab.Screen name="알람" component={Exampage} />
-      <Tab.Screen name="마이페이지" component={Mypage} />
-    </Tab.Navigator>
-  );
+    const dispatch = useDispatch();
+    const navigation = useNavigation();
+    const { userDataFetch } = useAxios();
+    useEffect(() => {
+        console.log('네비바 진입');
+        const fetchUserData = async () => {
+            // 사용자 정보 가져오기
+            const userInfo = await userDataFetch('GET', USER); // USER URL로 요청
+            console.log(userInfo); // 사용자 데이터 확인
+            if (userInfo) {
+                dispatch(setUserInfo(userInfo)); // 사용자 정보를 Redux에 저장
+                console.log('WellSleep 로그인 성공');
+            } else {
+                console.log('데이터없음');
+                navigation.navigate('Login');
+            }
+        };
+        console.log('사용자 데이터 확인');
+        fetchUserData();
+    }, []);
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                tabBarStyle: {
+                    backgroundColor: '#8B728F',
+                    borderTopWidth: 0,
+                    height: 70, // 탭 바의 높이 설정
+                },
+                tabBarLabelStyle: {
+                    color: '#ffffff', // 글씨 색상
+                    fontSize: 12, // 원하는 글씨 크기로 변경
+                    fontWeight: 'bold', // 글씨 두께
+                },
+                tabBarItemStyle: {
+                    paddingVertical: 10, // 각 탭의 수직 패딩
+                },
+                tabBarActiveTintColor: '#FFFFFF', // 선택된 탭의 색상
+                tabBarInactiveTintColor: '#C6BAC8', // 비활성 탭의 색상
+                headerShown: false, // 모든 스크린에서 헤더를 숨기려면 추가
+            }}
+        >
+            <Tab.Screen name="홈" component={Main} />
+            <Tab.Screen name="수면일지" component={MonthlyChart} />
+            <Tab.Screen name="알람" component={Exampage} />
+            <Tab.Screen name="마이페이지" component={Mypage} />
+        </Tab.Navigator>
+    );
 }
 
 export default NavBar;
--- frontend/types/navigation.d.ts
+++ frontend/types/navigation.d.ts
@@ -1,24 +1,20 @@
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 export type StackParamList = {
-  Main: undefined;
-  Mypage: undefined; // Mypage
-  Luck: undefined;
-  SleepLab: undefined;
-  Info: undefined;
-  MonthlyChart: undefined;
-  DailyChart: { totalInformationId: string };
+    Nav: undefined;
+    Main: undefined;
+    Login: undefined;
+    Mypage: undefined; // Mypage
+    Luck: undefined;
+    SleepLab: undefined;
+    Info: undefined;
+    MonthlyChart: undefined;
+    DailyChart: { totalInformationId: string };
 };
 
-export type MainNavigationProp = NativeStackNavigationProp<
-  StackParamList,
-  "Main"
->;
-export type MypageNavigationProp = NativeStackNavigationProp<
-  StackParamList,
-  "Mypage"
->;
+export type MainNavigationProp = NativeStackNavigationProp<StackParamList, 'Main'>;
+export type MypageNavigationProp = NativeStackNavigationProp<StackParamList, 'Mypage'>;
 export type StackParamList = {
-  MonthlyChart: undefined;
-  DailyChart: { totalInformationId: string };
+    MonthlyChart: undefined;
+    DailyChart: { totalInformationId: string };
 };
